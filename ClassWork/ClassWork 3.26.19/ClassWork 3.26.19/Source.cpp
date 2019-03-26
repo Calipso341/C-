968a476fc98a9523027428c53ce799b37485197a
Tatarintsev Nikita <Tatarintsev@step.itstep.rv.ua>
2019-03-26 14:48:14 +0200

    Structures

--- ClassWork/ClassWork 3.26.19/ClassWork 3.26.19/Source.cpp
+++ ClassWork/ClassWork 3.26.19/ClassWork 3.26.19/Source.cpp
@@ -3,85 +3,32 @@
 
 using namespace std;
 
-struct Address {
-
-	string country;
-	string region;
-	string town;
-	string street;
-	int zipCode;
-
-	void ShowAddressInfo() {
-
-		cout << "\n\tCountry: " << country << "\n\tRegion: " << region << "\n\tTown: " << town << "\n\tStreet: " << street << "\n\tZipCode: " << zipCode << endl;
-	};
-
-};
-struct Person {
-private:
+struct Animal {
 	string name;
-	string surname;
-	int age;
-public:
+	string Class;
+	string Name;
 	void ShowInfo() {
-		cout << "\n\tName: " << name << "\n\tSurname: " << surname << "\n\tAge: " << age << endl;
-	}
-	void SetAge(int newAge) {
-		if (newAge >= 120 || newAge <= 0) {
-			cout << "\n\tIncorrect age!" << endl;
-		}
-		else {
-			age = newAge;
-		}
-	}
-	int GetAge() {
-		return age;
-	}
-	void SetName(string newName) {
-
-		name = newName;
-
-	}
-	void SetSurname(string newSurname) {
-
-		surname = newSurname;
-
-	}
-	Address address;
+		cout << "\n\n\tName: " << name << "\n\n\tClass: " << Class << "\n\n\tName: " << Name << endl;
+	}	
 };
 
 int main() {
 
-	Person Bill;
-	/*Bill.name = "Bill";
-	Bill.surename = "Gates";
-	Bill.age = 24;*/
-	Bill.SetName("Bill");
-	Bill.SetSurname("Gates");
-	Bill.SetAge(57);
-	Bill.ShowInfo();
-	Bill.address.country = "USA";
-	Bill.address.region = "California";
-	Bill.address.town = "LA";
-	Bill.address.street = "S.Age";
-	Bill.address.zipCode = 123124;
-	Bill.address.ShowAddressInfo();
+    Animal Dog;
+	Dog.name = "Dog";
+	Dog.Class = "Canidae";
+	Dog.Name = "Bill";
+	Dog.ShowInfo();
 	//cout << "\n\t" << Bill.GetAge() << endl << endl;
 	cout << "\n\t===========================================================" << endl;
-	Person John;
+	Animal Pig;
 	/*Bill.name = "Bill";
 	Bill.surename = "Gates";
 	Bill.age = 24;*/
-	Bill.SetName("John");
-	Bill.SetSurname("Wick");
-	Bill.SetAge(57);
-	Bill.ShowInfo();
-	Bill.address.country = "USA";
-	Bill.address.region = "DC";
-	Bill.address.town = "Washinghton";
-	Bill.address.street = "S.Gop";
-	Bill.address.zipCode = 123124;
-	Bill.address.ShowAddressInfo();
+	Pig.name = "Pig";
+	Pig.Class = "Mammalia";
+	Pig.Name = "John";
+	Pig.ShowInfo();
 
 	system("pause");
 	return 0;
